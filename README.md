6af7f290d6557f63493d2ca85f960281fd2723be
Austin Kludass <37199952+austinkludass@users.noreply.github.com>
2025-06-20 16:26:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Web Template Repo
+Template repo for setting up a vanilla HTML/JS project
---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+
+export default defineConfig([
+  globalIgnores(["webpack.config.js"]),
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
